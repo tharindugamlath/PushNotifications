26dbe5f48796e1e063ee61ddf0c118d2dd71f292
Gama's Macmini <gamlath@keeneye.solutions>
2018-01-12 11:40:55 +0530

    first commit

---
+++ RemoteTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteTest
+//
+//  Created by Gama's Macmini on 1/9/18.
+//  Copyright © 2018 ShopFlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
